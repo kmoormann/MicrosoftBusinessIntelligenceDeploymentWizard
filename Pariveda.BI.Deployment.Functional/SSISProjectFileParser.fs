ffb8fb382fc35cd7587f31ffa886443038327a52
kmoormann <kevin.moormann@parivedasolutions.com>
2011-12-20 15:51:54 -0600

    adding new file

---
+++ Pariveda.BI.Deployment.Functional/SSISProjectFileParser.fs
@@ -0,0 +1,17 @@
+﻿
+
+namespace Pariveda.BI.Deployment.Library
+open System
+open System.IO
+open System.Xml
+
+
+    module SSISProjectFileParser =
+
+        let ParseSSISProjectFile projFilePath =
+            let projFile = new FileInfo(projFilePath)
+            let doc = new XmlDocument()
+            do doc.Load(projFile.FullName)
+            seq {for path in doc.SelectNodes("/Project/DTSPackages/DtsPackage/Name")
+                    -> new FileInfo(projFile.Directory.FullName + "\\" + path.InnerText) }
+
