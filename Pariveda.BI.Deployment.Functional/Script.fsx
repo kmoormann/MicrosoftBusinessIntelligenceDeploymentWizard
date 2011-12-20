a92980bccaf1c3b3f353eaa4773940243e212518
kmoormann <kevin.moormann@parivedasolutions.com>
2011-12-20 15:52:41 -0600

    updating parsers and exploratory "unit tests"

--- Pariveda.BI.Deployment.Functional/Script.fsx
+++ Pariveda.BI.Deployment.Functional/Script.fsx
@@ -2,6 +2,33 @@
 // It can be used to explore and test the library project.
 // Note that script files will not be part of the project build.
 
-#load "Module1.fs"
-open Module1
+#load "BISolutionParser.fs"
+open Pariveda.BI.Deployment.Library
 
+    
+
+//let ParseProjectFile fileName =
+//    let file = new FileInfo(fileName)
+//    match (file.Extension) with
+//        |".dbp"-> "database"
+//        |".dtproj" -> "integration"
+//        |".dwproj" -> "analysis"
+//        |".rptproj" -> "reports"
+//        |_ -> "not supported"
+//
+//let GetQuoted a =
+//    let pattern = @"([""'])(?:(?=(\\?))\2.)*?\1"
+//    let quoted = (new Regex(pattern)).Match(a).Value
+//    quoted.Replace("\"","")
+//
+//let ParseDatabaseProjectFile file =
+//    let lines = System.IO.File.ReadAllLines(file)|>Seq.map (fun x -> x.Trim())
+//                |>List.p
+//    let mutable scripts = List.empty<string>
+//    let mutable folderName = "none"
+//    for line in lines do
+//            folderName <- if line.StartsWith("Begin Folder") then GetQuoted line else folderName
+//            scripts <- if line.StartsWith("Script") then List.append scripts (GetQuoted line) else scripts
+//    folderName
+//    
+    
--- Pariveda.BI.Deployment.Library.Functional.Test/BISolutionsParserUnitTests.cs
+++ Pariveda.BI.Deployment.Library.Functional.Test/BISolutionsParserUnitTests.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.IO;
+using System.Text.RegularExpressions;
+using System.Xml;
 
 namespace Pariveda.BI.Deployment.Library.Functional.Test
 {
@@ -11,17 +13,17 @@ namespace Pariveda.BI.Deployment.Library.Functional.Test
     public class BISolutionsParserUnitTests
     {
         [TestMethod]
-        public void GetAllProjectsFromSolutionFile()
+        public void GetAllSSISProjectXMLFromSolutionFile()
         {
             //Arrange
             var slnFile = new FileInfo(@"C:\ParivedaDW_Projects\ParivedaAWDW\ParivedaAWDW.sln");
             //Act
             var projectFiles = SolutionParser.GetProjectXmlDocs(slnFile.FullName, ".dtproj");
-                //.ParseSolutionFile(slnFile.FullName,".dtproj");
+
             //Assert
             foreach (var item in projectFiles)
             {
-                Console.WriteLine(item.InnerXml);//ToString());
+
             }
             Assert.IsTrue(false);
         }
@@ -41,6 +43,40 @@ namespace Pariveda.BI.Deployment.Library.Functional.Test
             //Assert
             
             Console.WriteLine(fileDown.InnerXml);
+        }
+
+        private string GetQuoted(string stringToParse)
+        {
+            string pattern = @"([""'])(?:(?=(\\?))\2.)*?\1";
+            return (new Regex(pattern)).Match(stringToParse).Value.Replace("\"", String.Empty);
+        }
+        [TestMethod]
+        public void parsedatabaseProject()
+        {
+            string folderName = "";
+            var scriptsList = new List<KeyValuePair<string, FileInfo>>();
+            foreach (var line in System.IO.File.ReadAllLines(@"C:\DeploymentsTemp\ProjectFiles\db.dbp"))
+            {
+                if (line.Trim().StartsWith("Begin Folder")) 
+                    folderName = GetQuoted(line.Trim());
+                if (line.Trim().StartsWith("Script"))
+                    scriptsList.Add(new KeyValuePair<string,FileInfo>(folderName, new FileInfo(GetQuoted(line.Trim()))));
+
+            }
+
+            foreach (var item in scriptsList)
+            {
+                Console.WriteLine(item.Key + "\t" + item.Value);
+            }
+            
+        }
+
+        [TestMethod]
+        public void parseSSISProject()
+        {
+            foreach (var item in SSISProjectFileParser.ParseSSISProjectFile(@"C:\DeploymentsTemp\ProjectFiles\ssis.dtproj"))
+                Assert.IsFalse(item.Exists);
+
         }
 
     }
